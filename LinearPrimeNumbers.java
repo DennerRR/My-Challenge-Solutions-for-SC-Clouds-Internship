7f1ce867dea4458baf3c4a8f5f6ac18bb5dcb7d8
Denner Ramiro Ribeiro <dennerramiror@gmail.com>
2023-09-27 17:29:31 -0300

    LinearPrimeNumbers solution

---
+++ LinearPrimeNumbers.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+public class LinearPrimeNumbers {
+// Function to find prime numbers up to n using a linear approach
+    public static List<Integer> findPrimesLinear(int n) {
+        List<Integer> primes = new ArrayList<>();
+        if (n < 2) {
+            return primes; // No primes less than 2
+        }
+        for (int current = 2; current <= n; current++) {
+            if (isPrime(current)) {
+                primes.add(current);
+            }
+        }
+        return primes;
+    }
+
+// Helper function to check if a number is prime
+    private static boolean isPrime(int num) {
+        if (num <= 2) {
+            return num == 2; // 2 is a prime number
+        }
+        if (num % 2 == 0) {
+            return false; // If divisible by 2, not prime
+        }
+        for (int divisor = 3; divisor * divisor <= num; divisor += 2) {
+            if (num % divisor == 0) {
+                return false; // If divisible by any smaller number, not prime
+            }
+        }
+        return true; // If no divisors found, it's prime
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char exitKey = 'q';
+        boolean shouldExit = false;
+
+        do {
+            try {
+                if (shouldExit) {
+                    break; // Exit the loop if 'q' is pressed
+                }
+
+                System.out.print("Enter a number (or press '" + exitKey + "' to quit): ");
+                int n = scanner.nextInt();
+
+                List<Integer> result = findPrimesLinear(n);
+                System.out.print("Primes up to " + n + ": ");
+                for (int prime : result) {
+                    System.out.print(prime + " ");
+                }
+                System.out.println();
+            } catch (InputMismatchException e) {
+                String input = scanner.next();
+                if (input.length() == 1 && input.charAt(0) == exitKey) {
+                    shouldExit = true; // Set shouldExit to true if 'q' was pressed
+                } else {
+                    System.out.println("Invalid input. Please enter a valid number.");
+                }
+            }
+        } while (shouldExit==false);
+
+        System.out.println("Program closed.");
+        scanner.close();
+    }
+}
